53d881a3d0963795e3020dd1ebba3c82e950df5e
alma marquez <almamarquez2003@gmail.com>
2023-10-23 16:13:27 -0300

    cenfermedad

---
+++ src/cDisease.py
@@ -0,0 +1,8 @@
+
+class Disease:
+    def __init__(self, symptom, colour, max_time, diseaseName, severity):
+        self.symptom=symptom
+        self.colour=colour
+        self.max_time=max_time
+        self.diseaseName=diseaseName
+        self.severity=severity
