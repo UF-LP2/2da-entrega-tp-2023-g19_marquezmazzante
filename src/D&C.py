6e16b1bd2ca5f7407deff3d230e8b1bfe143bf8c
Lorenzo Mazzante <lorenzomazzante@hotmail.com.ar>
2023-10-23 17:24:28 -0300

    who.shall.I.take.care.of

---
+++ src/D&C.py
@@ -0,0 +1,26 @@
+from cPacient import cPacient
+from cNurse import cNurse
+
+def who_shall_I_Take_Care_of(lista_pac: list[cPacient],init: int,end: int) -> cPacient:
+
+    if (end - init == 0):                                       ##tengo un solo paciente en la lista
+        return lista_pac[init-1]
+
+    elif (end - init == 1):                                     ##tengo dos pacientes en la lista
+        coef1 = lista_pac[init-1].disease.severity / lista_pac[init-1].timeremaining()
+        coef2 = lista_pac[end-1].disease.severity / lista_pac[end-1].timeremaining()
+        if (coef1 > coef2):
+            return lista_pac[init-1]
+        else:
+            return lista_pac[end-1]
+
+    else:                                                       ##tengo mas de 2 pacientes en la lista
+        half = (end - init) // 2 + init
+        pac_aux1 = who_shall_I_Take_Care_of(lista_pac, init, half)
+        pac_aux2 = who_shall_I_Take_Care_of(lista_pac, half + 1, end)
+        coef1 = pac_aux1.disease.severity / pac_aux1.timeremaining()
+        coef2 = pac_aux2.disease.severity / pac_aux2.timeremaining()
+        if (coef1 > coef2):
+            return pac_aux1
+        else:
+            return pac_aux2
