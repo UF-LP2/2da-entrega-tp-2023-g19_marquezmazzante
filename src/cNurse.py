6a8f397ab72af06a0dee62a71efa68ce5ed417b5
Lorenzo Mazzante <lorenzomazzante@hotmail.com.ar>
2023-10-23 16:11:35 -0300

    creo clases

---
+++ src/cNurse.py
@@ -0,0 +1,7 @@
+class cNurse:
+    def __init__(self,name,surname,DNI,licence):
+        self.name = name
+        self.surname = surname
+        self.DNI = DNI
+        self.licence = licence
+
---
+++ src/cPacient.py
@@ -0,0 +1,7 @@
+class cPacient:
+    def __init__(self,name,surname,DNI,code,disease):
+        self.name = name
+        self.surname = surname
+        self.DNI = DNI
+        self.code = code
+        self.disease = disease
