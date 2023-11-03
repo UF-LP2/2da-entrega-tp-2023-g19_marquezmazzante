70f08cbaf66ba8cd3c675cbd9801c1706d667810
Lorenzo Mazzante <lorenzomazzante@hotmail.com.ar>
2023-11-03 16:39:44 -0300

    main andando y con sentido

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from src.readFiles import readNurses
 from src.readFiles import readPacients
 from datetime import datetime
+from library.cPacient import cPacient
+from library.diagnose import diagnose
+from library.cConsul import cConsul
+from library.attention import who_shall_I_Take_Care_of
+import time
 
 NConsulMax = 10
 
@@ -19,11 +24,38 @@ def main_divide_and_conquer() -> None:
     else:                                                      #turno noche
         nNurses = 1
 
+    listWaiting: list[cPacient] = []
+    listAtention: list[cPacient] = []
+    listConsul: list[cConsul] = [cConsul(1),cConsul(2),cConsul(3),cConsul(4),cConsul(5),cConsul(6),cConsul(7),cConsul(8),cConsul(9),cConsul(10)]
 
+    while(True):
 
+        for i in range (len(listPacients)):
+            pacientaux: cPacient = listPacients.pop(0)
+            dummy = diagnose(pacientaux)
 
+            if pacientaux.colour == 5:
+                listAtention.append(pacientaux)
+            else:
+                listWaiting.append(pacientaux)
 
+        consulaux = (len(listWaiting) // 10) + 1
+        if (consulaux >= NConsulMax):
+            Nconsul_open = NConsulMax
+        else:
+            Nconsul_open = consulaux
 
+        for j in range(Nconsul_open):
+            if listConsul[j].occupied == False:
+                pacientaux = who_shall_I_Take_Care_of(listWaiting,0,len(listWaiting))
+                listWaiting.remove(pacientaux)
+                listAtention.append(pacientaux)
+                print("se atendio a",pacientaux.name)
+                listConsul[j].empty_consul()
+            if listConsul[0] == None:
+                break
+
+        time.sleep(1)
 
 
 if __name__ == "__main__":
