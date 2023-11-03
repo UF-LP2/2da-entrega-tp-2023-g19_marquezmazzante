779506fc0aa01a952b748d221405288cbc7ae5ed
Lorenzo Mazzante <lorenzomazzante@hotmail.com.ar>
2023-11-03 16:29:49 -0300

    correcciones

--- library/attention.py
+++ library/attention.py
@@ -6,9 +6,9 @@ def who_shall_I_Take_Care_of(lista_pac: list[cPacient],init: int,end: int) -> cP
         return lista_pac[init-1]
 
     elif (end - init == 1):                                     ##tengo dos pacientes en la lista
-        coef1 = lista_pac[init-1].colour.value / lista_pac[init-1].timeremaining()
-        coef2 = lista_pac[end-1].colour.value / lista_pac[end-1].timeremaining()
-        if (coef1 > coef2):
+        coef1 = lista_pac[init-1].colour / lista_pac[init-1].timeremaining()
+        coef2 = lista_pac[end-1].colour / lista_pac[end-1].timeremaining()
+        if (coef1 >= coef2):
             return lista_pac[init-1]
         else:
             return lista_pac[end-1]
@@ -17,9 +17,9 @@ def who_shall_I_Take_Care_of(lista_pac: list[cPacient],init: int,end: int) -> cP
         half = (end - init) // 2 + init
         pac_aux1 = who_shall_I_Take_Care_of(lista_pac, init, half)
         pac_aux2 = who_shall_I_Take_Care_of(lista_pac, half + 1, end)
-        coef1 = pac_aux1.colour.value / pac_aux1.timeremaining()
-        coef2 = pac_aux2.colour.value / pac_aux2.timeremaining()
-        if (coef1 > coef2):
+        coef1 = pac_aux1.colour / pac_aux1.timeremaining()
+        coef2 = pac_aux2.colour / pac_aux2.timeremaining()
+        if (coef1 >= coef2):
             return pac_aux1
         else:
             return pac_aux2
