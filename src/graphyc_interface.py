13f744ea1efe8269f8aba45b1fbf183e2f51aab1
Lorenzo Mazzante <lorenzomazzante@hotmail.com.ar>
2023-11-06 23:08:39 -0300

    no se q hice pero esta divino

--- src/graphyc_interface.py
+++ src/graphyc_interface.py
@@ -3,7 +3,8 @@ from PyQt6.QtGui import QPainter, QPen, QColor
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QListWidget
 
-class cProyect():
-    print("hello world")
+def listainterfaz(self):
+    print ("hello world")
+
 
 
--- src/windows.py
+++ src/windows.py
@@ -1,7 +1,24 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QSpinBox, QWidget, QPushButton, QGridLayout, QSlider, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QSpinBox, QWidget, QPushButton, QGridLayout, QSlider, QLabel, QListWidget, QTextBrowser, QListWidgetItem
 from PyQt6.QtCore import Qt
-from src.graphyc_interface import cProyect
+from PyQt6.QtGui import QTextCursor, QTextCharFormat, QColor
 import sys
+from src.readFiles import readPacients
+from library.cPacient import cPacient
+from library.cColour import cColour
+from library.cNurse import cNurse
+
+
+def convert(coloraux: cColour) -> QColor:
+    if (coloraux.value == 5):
+        return QColor("red")
+    elif (coloraux.value == 4):
+        return QColor("orange")
+    elif (coloraux.value == 3):
+        return QColor("yellow")
+    elif (coloraux.value == 2):
+        return QColor("green")
+    else:
+        return QColor("blue")
 
 class Window(QMainWindow):      #windget principal
 
@@ -20,9 +37,26 @@ class Window(QMainWindow):      #windget principal
         self.__left = 300
         self.__width = 1000
         self.__height = 700
+        listitaaux: list[cPacient] = readPacients("DATA_PACIENT.csv")
+        enfermeritoaux = cNurse("carlos", "perazzo", "2111212", "safffdfdsfds")
 
         #inicializacion de widgets
         self.__labelTitle = QLabel("")
+        self.__list_widget = QListWidget()
+        self.__text_browser = QTextBrowser()
+
+
+        for i in range (len(listitaaux)):
+            dummy = enfermeritoaux.diagnose(listitaaux[i])
+            paciente = listitaaux[i]
+            nombre = paciente.name
+            color = paciente.colour
+
+            item = QListWidgetItem(nombre)
+
+            item.setForeground(convert(color))
+
+            self.__list_widget.addItem(item)
 
 
         #geometria de la ventana principal
@@ -41,11 +75,13 @@ class Window(QMainWindow):      #windget principal
 
         #agrego los widgets al layout
         layout.addWidget(self.__labelTitle, 8, 0, 1, 1)
+        layout.addWidget(self.__list_widget, 20, 0, 1, 1)
 
 
         #Configuraciones del widget central y del layout
         widgetCentral.setLayout(layout)
         self.setCentralWidget(widgetCentral)
+
 
 
 
